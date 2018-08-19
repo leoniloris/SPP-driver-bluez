07f64f156fca758835676f9936476c2e419c22a7
leoni <leoniloris1@hotmail.com>
2018-08-19 23:03:53 +0000

    Custom bluez service emulating a SPP GATT server.
    Adds a simple char driver

--- ble_io.c
+++ ble_io.c
@@ -5,22 +5,19 @@
 #include <linux/slab.h>
 
 #define BUFFER_SIZE 1024
+#define MAJOR_NUMBER 7
 
 MODULE_LICENSE("GPL");
 
-static int place_in_buffer = 0;
-static int end_of_buffer = 1024;
-static int MAJOR_NUMBER = 240;
-char *DEVICE_NAME = "simple_char_driver";
-typedef struct {
-  char *buf;
-} buffer;
+static int buffer_offset = 0;
+
+char *DEVICE_NAME = "char_driver";
 
 char *device_buffer;
 static int closeCounter = 0;
 static int openCounter = 0;
 
-ssize_t simple_char_driver_read(struct file *pfile, char __user *buffer,
+ssize_t char_driver_read(struct file *pfile, char __user *buffer,
                                 size_t length, loff_t *offset) {
   if (*offset > BUFFER_SIZE) {
     printk("offset is greater than buffer size");
@@ -36,14 +33,8 @@ ssize_t simple_char_driver_read(struct file *pfile, char __user *buffer,
   return length;
 }
 
-ssize_t simple_char_driver_write(struct file *pfile, const char __user *buffer,
+ssize_t char_driver_write(struct file *pfile, const char __user *buffer,
                                  size_t length, loff_t *offset) {
-  /* *buffer is the userspace buffer where you are writing the data you want to
-   * be written in the device file*/
-  /* length is the length of the userspace buffer*/
-  /* current position of the opened file*/
-  /* copy_from_user function: destination is device_buffer and source is the
-   * userspace buffer *buffer */
   int nb_bytes_to_copy;
   if (BUFFER_SIZE - 1 - *offset <= length) {
     nb_bytes_to_copy = BUFFER_SIZE - 1 - *offset;
@@ -64,7 +55,7 @@ ssize_t simple_char_driver_write(struct file *pfile, const char __user *buffer,
   return nb_bytes_to_copy;
 }
 
-int simple_char_driver_open(struct inode *pinode, struct file *pfile) {
+int char_driver_open(struct inode *pinode, struct file *pfile) {
   printk(KERN_ALERT "This device is now open");
   openCounter++;
   printk(KERN_ALERT "This device has been opened this many times: %d\n",
@@ -72,7 +63,7 @@ int simple_char_driver_open(struct inode *pinode, struct file *pfile) {
   return 0;
 }
 
-int simple_char_driver_close(struct inode *pinode, struct file *pfile) {
+int char_driver_close(struct inode *pinode, struct file *pfile) {
   printk(KERN_ALERT "This device is now closed");
   closeCounter++;
   printk(KERN_ALERT "This device has been closed this many times: %d\n",
@@ -80,14 +71,14 @@ int simple_char_driver_close(struct inode *pinode, struct file *pfile) {
   return 0;
 }
 
-loff_t simple_char_driver_seek(struct file *pfile, loff_t offset, int whence) {
+loff_t char_driver_seek(struct file *pfile, loff_t offset, int whence) {
   printk(KERN_ALERT "We are now seeking!");
   switch (whence) {
   case 0: {
-    if (offset <= end_of_buffer && offset > 0) {
-      place_in_buffer = offset;
+    if (offset <= BUFFER_SIZE && offset > 0) {
+      buffer_offset = offset;
       printk(KERN_ALERT " this is where we are in the buffer: %d\n",
-             place_in_buffer);
+             buffer_offset);
     } else {
       printk(KERN_ALERT "ERROR you are attempting to go ouside the Buffer");
     }
@@ -95,21 +86,21 @@ loff_t simple_char_driver_seek(struct file *pfile, loff_t offset, int whence) {
   }
 
   case 1: {
-    if (((place_in_buffer + offset) <= end_of_buffer) &&
-        ((place_in_buffer + offset) > 0)) {
-      place_in_buffer = place_in_buffer + offset;
+    if (((buffer_offset + offset) <= BUFFER_SIZE) &&
+        ((buffer_offset + offset) > 0)) {
+      buffer_offset = buffer_offset + offset;
       printk(KERN_ALERT " this is where we are in the buffer: %d\n",
-             place_in_buffer);
+             buffer_offset);
     } else {
       printk(KERN_ALERT "ERROR you are attempting to go ouside the Buffer");
     }
     break;
   }
   case 2: { // THIS IS SEEK END
-    if ((end_of_buffer - offset) >= 0 && offset > 0) {
-      place_in_buffer = end_of_buffer - offset;
+    if ((BUFFER_SIZE - offset) >= 0 && offset > 0) {
+      buffer_offset = BUFFER_SIZE - offset;
       printk(KERN_ALERT " this is where we are in the buffer: %d\n",
-             place_in_buffer);
+             buffer_offset);
     } else {
       printk(KERN_ALERT "ERROR you are attempting to go ouside the Buffer");
     }
@@ -118,31 +109,31 @@ loff_t simple_char_driver_seek(struct file *pfile, loff_t offset, int whence) {
   default: {}
   }
   printk(KERN_ALERT "I sought %d\n", whence);
-  return place_in_buffer;
+  return buffer_offset;
 }
 
-struct file_operations simple_char_driver_file_operations = {
+struct file_operations char_driver_file_operations = {
     .owner = THIS_MODULE,
-    .read = simple_char_driver_read,
-    .write = simple_char_driver_write,
-    .open = simple_char_driver_open,
-    .llseek = &simple_char_driver_seek,
-    .release = simple_char_driver_close,
+    .read = char_driver_read,
+    .write = char_driver_write,
+    .open = char_driver_open,
+    .llseek = &char_driver_seek,
+    .release = char_driver_close,
 };
 
-static int simple_char_driver_init(void) {
+static int char_driver_init(void) {
   printk(KERN_ALERT "inside %s function\n", __FUNCTION__);
   register_chrdev(MAJOR_NUMBER, DEVICE_NAME,
-                  &simple_char_driver_file_operations);
+                  &char_driver_file_operations);
   device_buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
   return 0;
 }
 
-static void simple_char_driver_exit(void) {
+static void char_driver_exit(void) {
   printk(KERN_ALERT "inside %s function\n", __FUNCTION__);
   unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
   kfree(device_buffer);
 }
 
-module_init(simple_char_driver_init);
-module_exit(simple_char_driver_exit);
+module_init(char_driver_init);
+module_exit(char_driver_exit);
